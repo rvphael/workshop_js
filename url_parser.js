1bf60bfea5d4ece94446088d2dc9ea300849f29e
Raphael Santos <raphaelsantos.j@gmail.com>
2019-08-22 19:07:26 -0300

    :beers: Get path

--- url_parser.js
+++ url_parser.js
@@ -1,6 +1,7 @@
 module.exports = function urlParser (url) {
   return {
     protocol: url.match(/(\w+):.*$/)[1],
-    hostname: url.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/)[1]
+    hostname: url.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/)[1],
+    path: url.match(/(http[s]?:\/\/)?([^\/\s]+\/)(.*)/)[3]
   };
 }
